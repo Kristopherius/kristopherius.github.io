1f255d001bf809d89ddf103393c47c483d9c11e8
Chris Donutello <kristopherius@gmail.com>
2026-01-16 13:46:11 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kristopherius.github.io',
+  base: '/my-repo',
   vite: {
     plugins: [tailwindcss()],
   },
