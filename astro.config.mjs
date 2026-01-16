ef2c5b8d3bf8761912894697efb0f68209967af6
Chris Donutello <kristopherius@gmail.com>
2026-01-16 16:13:07 +0100

    Fix formatting of base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kristopherius.github.io',
-  base: '/',
+  base: '/' ,
   vite: {
     plugins: [tailwindcss()],
   },
