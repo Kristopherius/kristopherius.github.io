2566028cdde1e0ce223627b941b22fd2d9857ccc
Chris Donutello <kristopherius@gmail.com>
2026-01-16 16:26:55 +0100

    Fix formatting of design cycle description in config.ts

--- src/config.ts
+++ src/config.ts
@@ -120,7 +120,7 @@ export const siteConfig = {
       dateRange: "Feb 2024 - Jun 2024",
       bullets: [
         "Created a gamified learning experience for teenagers to understand the basic concept of programming in C#.",
-        "Mastered the "Orientation – Conceptualization – Prototyping – Evaluation" design cycle.",
+        "Mastered the ""Orientation – Conceptualization – Prototyping – Evaluation"" design cycle.",
         "Effectively managed client relationship with museum employees. Transforming the client's needs into a usable end product through continuous feedback and collaboration.",
       ],
     },
